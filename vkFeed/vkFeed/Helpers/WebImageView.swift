ba183305a63edec18352138215c97a9f7b8dc63d
 Evgeniy <spiralcka@gmail.com>
2019-05-28 16:58:35 +0300

    imageLoader added

---
+++ vkFeed/vkFeed/Helpers/WebImageView.swift
@@ -0,0 +1,39 @@
+//
+//  WebImageView.swift
+//  vkFeed
+//
+//  Created by  Евгений on 28/05/2019.
+//  Copyright © 2019 LosAnatoly Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WebImageView: UIImageView {
+    func set(imageURL: String) {
+        guard let url = URL(string: imageURL) else { return }
+        
+        if let cachedResponse = URLCache.shared.cachedResponse(for: URLRequest(url: url)) {
+            self.image = UIImage(data: cachedResponse.data)
+            print("From cache")
+            return
+        }
+        print("From internet")
+
+        let dataTask = URLSession.shared.dataTask(with: url) { [weak self](data, response, error) in
+            DispatchQueue.main.async {
+                if let data = data, let response = response {
+                    self?.image = UIImage(data: data)
+                    self?.handleLoadedImage(data: data, response: response)
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    
+    private func handleLoadedImage(data: Data, response: URLResponse) {
+        guard let responseURL = response.url else { return }
+        let cachedResponse = CachedURLResponse(response: response, data: data)
+        URLCache.shared.storeCachedResponse(cachedResponse, for: URLRequest(url: responseURL))
+    }
+}
--- vkFeed/vkFeed/NewsFeed/Cell/NewsFeedCell.swift
+++ vkFeed/vkFeed/NewsFeed/Cell/NewsFeedCell.swift
@@ -28,7 +28,7 @@ protocol FeedCellViewModel {
 
 class NewsFeedCell: UITableViewCell {
     
-    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var iconImageView: WebImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var postLabel: UILabel!
@@ -44,6 +44,7 @@ class NewsFeedCell: UITableViewCell {
     }
     
     func set(viewModel: FeedCellViewModel) {
+        iconImageView.set(imageURL: viewModel.iconUrlString)
         nameLabel.text = viewModel.name
         dateLabel.text = viewModel.date
         postLabel.text = viewModel.text
