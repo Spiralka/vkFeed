c783fd3b67f80d137d1c73f8e707e0c1b2081f94
 Evgeniy <spiralcka@gmail.com>
2019-06-13 19:55:24 +0300

    bugFixes and dataDetectorTypes

--- vkFeed/vkFeed/Helpers/WebImageView.swift
+++ vkFeed/vkFeed/Helpers/WebImageView.swift
@@ -10,7 +10,11 @@ import Foundation
 import UIKit
 
 class WebImageView: UIImageView {
+    
+    private var currentUrlString: String?
+    
     func set(imageURL: String?) {
+        currentUrlString = imageURL
         guard let imageURL = imageURL, let url = URL(string: imageURL) else { self.image = nil; return}
         
         if let cachedResponse = URLCache.shared.cachedResponse(for: URLRequest(url: url)) {
@@ -21,7 +25,6 @@ class WebImageView: UIImageView {
         let dataTask = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
             DispatchQueue.main.async {
                 if let data = data, let response = response {
-                    self?.image = UIImage(data: data)
                     self?.handleLoadedImage(data: data, response: response)
                 }
             }
@@ -33,5 +36,8 @@ class WebImageView: UIImageView {
         guard let responseURL = response.url else { return }
         let cachedResponse = CachedURLResponse(response: response, data: data)
         URLCache.shared.storeCachedResponse(cachedResponse, for: URLRequest(url: responseURL))
+        if responseURL.absoluteString == currentUrlString {
+            self.image = UIImage(data: data)
+        }
     }
 }
